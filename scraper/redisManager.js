e69bde4192bf16f0fca6c3e7b86bb7030f5e9ea0
dragosb112 <dragos.budan@gmail.com>
2017-05-27 20:12:32 +0100

    Added logging for the redis manager

--- scraper/redisManager.js
+++ scraper/redisManager.js
@@ -1,10 +1,14 @@
 var redis = require('redis');
 var appManager = require('./appManager.js');
+var logger = require('./utilities/logger.js');
 
 var channelName = 'server';
-var subscriber = redis.createClient();
+var subscriber = null; 
 
 function initialise() {
+    logger.info('redisManager: initialise');
+    subscriber = redis.createClient();
+    logger.info('redisManager: created subscriber client');
     subscriber.on('message', function (channel, message) {
         if (channel === channelName) {
             switch (message) {
@@ -23,8 +27,8 @@ function initialise() {
             }
         }
     });
-
     subscriber.subscribe(channelName);
+    logger.info('redisManager: subscribed to ' + channelName);
 }
 
 module.exports = {
