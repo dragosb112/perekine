9eee84e9d932fc40058d9b6e4a14cb3cbb2366af
dragosb112 <dragos.budan@gmail.com>
2017-05-28 20:16:52 +0100

    Added creation of lib folder for angular on server

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,11 @@ mkdir -p ./scraper/output/twitter;
 
 #server
 mkdir -p ./server/output/logs;
+mkdir -p ./server/public/javascripts/lib;
 
 #download dependencies
-wget -O ./server/public/javascripts/lib/angular.min.js "https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"
-wget -O ./server/public/javascripts/lib/angular.min.js.map "https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js.map"
+wget -O ./server/public/javascripts/lib/angular.min.js "https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js";
+wget -O ./server/public/javascripts/lib/angular.min.js.map "https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js.map";
 
 #resolve npm dependencies
 npm install;
